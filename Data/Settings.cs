0d2ec568a941a737c5233ff50aae9de578cec8b4
justice586 <traviswylie58@gmail.com>
2018-11-04 20:08:22 +1300

    slight optomizations started adding comments through code

--- Data/Settings.cs
+++ Data/Settings.cs
@@ -5,15 +5,17 @@ namespace Justibot
 {
     public class Settings
     {
+        //list of server IDs where servers are in bunker mode
         public static List<ulong> bunker = new List<ulong>();
 
+        //note module limitations
         public static int maxUserNotes = 10;
         public static int maxStaffNotes = 20;
         public static int maxServerNotes = 30;
         public static int maxNoteLength = 150;
         public static int maxNameLength = 25;
         
-
+        //default server blacklist
         public static ulong[] defaultblacklist =
         {
             110373943822540800,
