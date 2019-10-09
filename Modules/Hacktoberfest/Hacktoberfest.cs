5826bd2a9ac4b9e470e57bc6b14d2afee92396e1
Travis Wylie <traviswylie58@gmail.com>
2019-10-09 13:18:15 +1300

    Added Hacktoberfest module

---
+++ Modules/Hacktoberfest/Hacktoberfest.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Justibot.Services;
+
+namespace Justibot.Modules.Help
+{
+    [Group("Hackfest")]
+    public class HacktoberModule : ModuleBase
+    {
+
+        // [Command("CommandName")] //single word
+        // [Summary("Description of command")] //can be multiple words
+        // public async Task MethodName() //method name should be related to command name
+        // {
+        //     string response;
+
+        //     //code to create response
+
+        //     await ReplyAsync(response);
+        // }
+        
+    }
+}
